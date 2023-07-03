c1456c878542877c58d087e96fe75683d39bcc3a
Brandon Shaffer <shafferbrandon10@hotmail.com>
2023-07-03 17:06:42 -0400

    The Moon dropdown works
    It is visible only when viewing a planet or a moon of the same planet. Information changes when selecting a moon.

--- Assets/Scripts/DropdownActivation.cs
+++ Assets/Scripts/DropdownActivation.cs
@@ -10,6 +10,7 @@ public class DropdownActivation : MonoBehaviour
     public static GameObject uiCanvas;
     public TMP_Dropdown viewDropdown;
     public TMP_Dropdown starTypeDropdown;
+    public TMP_Dropdown moonDropdown;
     private void Start()
     {
         uiCanvas = GameObject.Find("ObjectInfoUI");
@@ -18,9 +19,14 @@ public class DropdownActivation : MonoBehaviour
         viewDropdown.interactable = false;
         viewDropdown.GetComponent<CanvasGroup>().alpha = 0;
 
+        moonDropdown = uiCanvas.transform.Find("Moon Selector").GetComponent<TMP_Dropdown>();
+        moonDropdown.interactable = false;
+        moonDropdown.GetComponent<CanvasGroup>().alpha = 0;
+
         starTypeDropdown = uiCanvas.transform.Find("StarTypeDropdown").GetComponent<TMP_Dropdown>();
         starTypeDropdown.interactable = true;
         starTypeDropdown.GetComponent<CanvasGroup>().alpha = 1;
+
     }
     
     public void ShowDropdown()
@@ -39,12 +45,18 @@ public class DropdownActivation : MonoBehaviour
 
         starTypeDropdown.interactable = false;
         starTypeDropdown.GetComponent<CanvasGroup>().alpha = 0;
+
+        moonDropdown.interactable = true;
+        moonDropdown.GetComponent<CanvasGroup>().alpha = 1;
     }
 
     public void RemoveDropdown()
     {
         viewDropdown.interactable = false;
         viewDropdown.GetComponent<CanvasGroup>().alpha = 0;
+
+        moonDropdown.interactable = false;
+        moonDropdown.GetComponent<CanvasGroup>().alpha = 0;
 
         starTypeDropdown.interactable = true;
         starTypeDropdown.GetComponent<CanvasGroup>().alpha = 1;
